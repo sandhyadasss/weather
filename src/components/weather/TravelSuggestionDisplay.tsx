d7a824822c3cd034c75a020e0ede4e1fda5696fb
sandhya das <sandhyad930@gmail.com>
2025-08-24 16:20:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/weather/TravelSuggestionDisplay.tsx
+++ src/components/weather/TravelSuggestionDisplay.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Icons } from '@/components/weather/icons';
